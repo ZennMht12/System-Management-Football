1bd84cc1eb9048a7b22c3b001f2017583e2e44e9
ZennMht12 <zeennxz12@gmail.com>
2023-01-07 01:51:45 +0700

    first commit

---
+++ dist/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"system_football_management"}];updateSearchResults();
---
+++ src/system_football_management/ConnectionDatabase2.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package system_football_management;
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+/**
+ * 
+ * @author DELL
+ */
+public class ConnectionDatabase2 {
+    private static Connection MySQLConfig;
+    public static Connection configDB()throws SQLException{
+        try{
+            String url = "jdbc:mysql://localhost:3306/db_login";
+            String user = "root";
+            String pass = "";
+            
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            MySQLConfig = DriverManager.getConnection(url, user, pass);
+            
+     
+                    
+        }catch (SQLException e){
+            
+            System.out.println("KONEKSI KE DATABASE GAGAL" + e.getMessage());
+            
+        }
+         return MySQLConfig; 
+    }    
+}
+
